b480a65fe9a1e914100dc8423108499684f4a19e
spaceturtle1018 <67324642+spaceturtle1018@users.noreply.github.com>
2025-08-05 11:30:25 +0900

    Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -5,5 +5,6 @@ import streamlit.components.v1 as components
 with open("index.html", "r", encoding="utf-8") as f:
     html_string = f.read()
 
-st.title("My Embedded HTML Page")
+st.title("Fintech")
 components.html(html_string, height=600, scrolling=True)
+
